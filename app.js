656d85a227fbb7595cd9c874ebc6886a74cd1770
jengerred <56004257+jengerred@users.noreply.github.com>
2022-10-07 18:12:39 -0400

    My custom Budget App
    No tutorial this time!

---
+++ app.js
@@ -0,0 +1,12 @@
+const num1 = document.getElementById("a.value");
+const num2 = document.getElementById("b").value;
+const num3 = document.getElementById("c").value;
+const num4 = document.getElementById("d").value;
+const num5 = document.getElementById("e").value;
+const num6 = document.getElementById("f").value;
+
+const num7 = document.getElementById("a.value");
+
+
+
+console.log(num7);
